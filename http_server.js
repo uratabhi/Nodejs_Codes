f752051f531192e0988868679b45beb718cc838c
Abhishek Jaiswal <waitabhi2015@gmail.com>
2023-07-27 15:30:02 +0530

    server is created where user can see their details

--- http_server.js
+++ http_server.js
@@ -1,22 +1,46 @@
 const http = require("http");
 
+const fs = require("fs");
+
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html')
-  console.log(req.url);
-  if(req.url=='/home'){
-     res.end('<h1> Welcome Home </h1>')
-  }
-  else if(req.url =='/about'){
-     res.end('<h1>Welcome to About Us Page </h1>')
-  }
-  else if(req.url=='/node'){
-    res.end('<h1>Welcome to my NodeJs project </h1>')
-  }
-  else{
-     res.end('<h1>server not found! </h1>');
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title></head>");
+      res.write(`<body>${data}</body>`)
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form><body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        const data = fs.readFileSync("message.txt");
+        return res.end();
+      });
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>hii </h1>");
   }
 });
 
-server.listen(4000, ()=>{
-     console.log('server is lisetening on port 4000')
+server.listen(4000, () => {
+  console.log("server is lisetening on port 4000");
 });
